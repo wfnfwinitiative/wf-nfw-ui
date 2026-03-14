a0ecf15babe87c5738b840851fd751cfb20db7fc
Sreeman Ambati <sreeman.ambati@gmail.com>
2026-03-14 11:45:52 +0530

    Added local backend

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ export const config = {
   llmServiceUrl: import.meta.env.VITE_LLM_SERVICE_URL || 'http://localhost:8000',
 
   // Backend Services API Base URL
-  serviceUrl: import.meta.env.VITE_BACKEND_SERVICE_URL || 'https://wf-nfw-services-g3ogd5xux-wf-nfws-projects.vercel.app',
+  serviceUrl: import.meta.env.VITE_BACKEND_SERVICE_URL || 'https://wf-nfw-services-huzmod1o0-wf-nfws-projects.vercel.app',
 };
 
 export default config;
