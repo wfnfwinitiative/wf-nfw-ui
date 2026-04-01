be67a7a3c79a3a59f592df511cc81c2b2248dde9
vsurendrav <surendra.kmr3@gmail.com>
2026-04-01 08:53:59 +0530

    removing not required changes

--- vite.config.js
+++ vite.config.js
@@ -15,13 +15,6 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    port: 3000
   }
 })
